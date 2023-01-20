9f90353dd6750bc58fee9b70a98ee566a2c2fe6d
raviyadav137 <rabiyadav137@gmail.com>
2023-01-20 15:21:59 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Main from "./cmp/Main";
+import Center from "./cmp/Center";
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+      <Center />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/cmp/Funcmp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Funcmp = () => {
+    return (
+        <div className='box2'>
+            <h1>This is created using Functional component</h1>
+            <p>This is done using external CSS</p>
+            <p>This is done using inline CSS</p>
+        </div>
+    )
+}
+
+export default Funcmp
